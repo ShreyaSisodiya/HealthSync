2272aff05d0fc322aa2172f7e238771f142293a8
ShreyaSisodiya <sisodiya.s@northeastern.edu>
2026-01-18 19:15:49 -0800

    chore: initialize Xcode project with SwiftUI and HealthKit
    :q

---
+++ HealthSync/HealthSync/HealthSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthSyncApp.swift
+//  HealthSync
+//
+//  Created by Shreya Sisodiya on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct HealthSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthSync/HealthSyncTests/HealthSyncTests.swift
@@ -0,0 +1,16 @@
+//
+//  HealthSyncTests.swift
+//  HealthSyncTests
+//
+//  Created by Shreya Sisodiya on 1/18/26.
+//
+
+import Testing
+
+struct HealthSyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
